953f18c2c20805589423011314e01d3f9fe55389
Shriyashzzz <ghimires1@my.smccd.edu>
2026-04-03 14:25:36 -0700

    workign logic

--- js.js
+++ js.js
@@ -1,214 +1,184 @@
-
-
-
 const gameBoard = (function(){
-    const boardArray = Array.from({length: 3}, () => Array(3).fill(undefined))
+    let boardArray = Array.from({length: 3}, () => Array(3).fill(undefined))
+    let even0ddCounter = 1;
+    let player1, player2;
 
     const setValue = (id, row, col, playerOwnInputArray) =>{
-        if(id ==1 ){
+        if(id == 1){
             boardArray[row][col] = "X"
             playerOwnInputArray[row][col] = id;
-        }
-
-        else if(id ==2){
+        } else if(id == 2){
             boardArray[row][col] = "O"
             playerOwnInputArray[row][col] = id;
-            }
+        }
         console.table(boardArray);
-        }
-        
-
-
-   
-    const getValueAt = (row, col) =>{
-            return (boardArray[row][col])
-        }
-     const checkIfOccupied = (row, col)=>{
-        if(getValueAt(row, col) === undefined){
-            return false; // false for unoccupiedhit index
-        }else{ 
-            return true; // true for occupied hit index
-        }
-
     }
 
+    const resetBoardArray = () =>{
+        boardArray = Array(3).fill().map(() => Array(3).fill(undefined));
+        even0ddCounter = 1;
+        resetPlayers(); // recreate players so their arrays are fresh
+        console.table(boardArray);
+    }
 
-    const invokeIfWon = (id,winningIndexString ) =>{
-        console.log(`player${id} won by choosing values at `,winningIndexString )
+    const getEvenOddCounter = () => even0ddCounter;
+    const incerementEvenOddCounter = () => even0ddCounter += 1;
+
+    const getValueAt = (row, col) => boardArray[row][col];
+
+    const checkIfOccupied = (row, col) =>{
+        return getValueAt(row, col) !== undefined;
+    }
+
+    const invokeIfWon = (id, winningIndexString) =>{
+        console.log(`player${id} won at`, winningIndexString)
         userUI.disableButton();
-        
+        userUI.setPlayerinfo(`Player ${id} wins!`);
     }
-    const checkIfWon = (id, givenBoard)  =>{
-        console.log("checkIfWon starting");
+
+    const checkIfWon = (id, givenBoard) =>{
         let diagonalSameSum = 0;
         for(let i = 0; i <= 2; i++){
-
-            //checks rows
-            let winningIndex = "";
-            let rowSum = 0; // starts at 0 to check for three counts
+            let rowSum = 0;
             let colSum = 0;
-                for(let j = 0; j <= 2; j++){
+            for(let j = 0; j <= 2; j++){
                 rowSum += givenBoard[i][j];
                 colSum += givenBoard[j][i];
-                if(i==j){
-                    diagonalSameSum += givenBoard[j][i];
-                }
-
-                if((rowSum == 3) || (rowSum==6)){
-                    winningIndex = `[${i}][0],[${i}][1],[${i}][2] `
-                    invokeIfWon(id,winningIndex )
-                    break;
-                }
-                else if((colSum == 3) || (colSum==6)){
-                    winningIndex = `[${i}][0],[${i}][1],[${i}][2] `
-                    invokeIfWon(id,winningIndex );
-                    break;
-                }
-
-                
-            }   
-            //check diagnals same values
-            if((diagonalSameSum == 3) || (diagonalSameSum==6)){
-                winningIndex = `[0][0],[1][1],[2][2] `
-                invokeIfWon(id,winningIndex );
-                return;
+                if(i == j) diagonalSameSum += givenBoard[i][j];
             }
-            //check diagnals opposite 
-                
-            let diagonalOppositeSum = givenBoard[0][2] + givenBoard[1][1] + givenBoard[2][0];
-            if((diagonalOppositeSum == 3) || (diagonalOppositeSum==6)){
-                winningIndex = `[0][2],[1][1],[2][0] `
-                invokeIfWon(id,winningIndex );
-                return;
+            if(rowSum == 3 || rowSum == 6){
+                invokeIfWon(id, `[${i}][0],[${i}][1],[${i}][2]`); return;
             }
+            if(colSum == 3 || colSum == 6){
+                invokeIfWon(id, `[0][${i}],[1][${i}],[2][${i}]`); return;
+            }
+        }
+        if(diagonalSameSum == 3 || diagonalSameSum == 6){
+            invokeIfWon(id, `[0][0],[1][1],[2][2]`); return;
+        }
+        let antiDiag = givenBoard[0][2] + givenBoard[1][1] + givenBoard[2][0];
+        if(antiDiag == 3 || antiDiag == 6){
+            invokeIfWon(id, `[0][2],[1][1],[2][0]`); return;
         }
     }
 
     function createPlayer(id){
         const playerid = id;
         const playerOwnInputArray = Array.from({length: 3}, () => Array(3).fill(undefined));
-
-        const inputValue = (arrayIndexRow, arrayIndexCol) => {
-            setValue(playerid,arrayIndexRow, arrayIndexCol, playerOwnInputArray)
-            checkIfWon(playerid, playerOwnInputArray)      
-            
+        const inputValue = (row, col) =>{
+            setValue(playerid, row, col, playerOwnInputArray);
+            checkIfWon(playerid, playerOwnInputArray);
         }
-        
-        
-    
-        return {playerid, inputValue, getValueAt} 
+        return {playerid, inputValue, getValueAt}
     }
 
-    const player1 = createPlayer(1);
-    const player2 = createPlayer(2);
-
-
-    return{player1, player2, checkIfOccupied};  // Documenting my mistakes with comments. Hence make sure it's in {} tags to ensure you are returning objects!
- 
-})(); // need () for IIFE
-
-
-
-
-const userUI =( function(){
-    const clickBtn = document.querySelectorAll(".cell");
-    let playerOneName = "";
-    let playerTwoName ="";
-    const gameInfo= document.querySelector(".gameInfo");
-
-    function startGame(){
-        let even0ddCounter = 1;
-        gameInfo.textContent = `${playerOneName} Turn`;
-        clickBtn.forEach((button) => button.addEventListener('click', ()=>{
-            let hitIndex = button.dataset.id.split(',');
-            if(gameBoard.checkIfOccupied(hitIndex[0], hitIndex[1])){
-                return
-            }
-            else if(even0ddCounter ==9){
-                disableButton();
-                setPlayerinfo("Draw")
-            }
-            else{
-                if(even0ddCounter%2 !=0){
-                    gameBoard.player1.inputValue(hitIndex[0], hitIndex[1])
-                    even0ddCounter +=1
-                    button.textContent = "X";
-                    setPlayerinfo(`${playerTwoName} Turn`)
-                    // player1.inputValue()
-                }else{
-                    gameBoard.player2.inputValue(hitIndex[0], hitIndex[1])
-                    button.textContent = "O";
-                    even0ddCounter+=1
-                    setPlayerinfo(`${playerOneName} Turn`)
-            }
-            }
-            
-        }))
-
-        
-               
+    const resetPlayers = () =>{
+        player1 = createPlayer(1);
+        player2 = createPlayer(2);
     }
 
-    const setPlayerinfo = (info) =>{
-            gameInfo.textContent = info;
-        }
+    resetPlayers();
 
-    const setPlayerName = (player1, player2) =>{
-        playerOneName = player1;
-        playerTwoName =player2;
-    }
-    const disableButton = () =>{
-            clickBtn.forEach((button) =>{
-                button.disabled =true;
-            })
-            console.log("game over")
-    }
-    const enableButtons =() =>{
-        clickBtn.forEach((button) =>{
-                button.disabled =false;
-        })
-        
-    }
-    return{startGame, disableButton, enableButtons, setPlayerName, setPlayerinfo, clickBtn}
-  
+    return { 
+        get player1(){ return player1; }, 
+        get player2(){ return player2; },
+        checkIfOccupied, resetBoardArray, getEvenOddCounter, incerementEvenOddCounter
+    };
 
 })();
 
-const uiController = (function(){
-    const startBtn = document.querySelector(".Start")
-    const resetBtn = document.querySelector(".Reset")
-    userUI.disableButton()
-    const submitBtn = document.querySelector(".submit-form-btn");
 
-    const formDialog = document.getElementById("userNameDialog")
-    startBtn.addEventListener('click', ()=>{
+const userUI = (function(){
+    const clickBtn = document.querySelectorAll(".cell");
+    let playerOneName = "";
+    let playerTwoName = "";
+    const gameInfo = document.querySelector(".gameInfo");
+
+    clickBtn.forEach((button) => button.addEventListener('click', () => {
+        let hitIndex = button.dataset.id.split(',');
+
+        if(gameBoard.checkIfOccupied(hitIndex[0], hitIndex[1])){
+            return;
+        }
+        if(gameBoard.getEvenOddCounter() == 10){
+            disableButton();
+            setPlayerinfo("Draw");
+            return;
+        }
+        if(gameBoard.getEvenOddCounter() % 2 != 0){
+            gameBoard.player1.inputValue(hitIndex[0], hitIndex[1]);
+            gameBoard.incerementEvenOddCounter();
+            button.textContent = "X";
+            setPlayerinfo(`${playerTwoName} Turn`);
+        } else {
+            gameBoard.player2.inputValue(hitIndex[0], hitIndex[1]);
+            button.textContent = "O";
+            gameBoard.incerementEvenOddCounter();
+            setPlayerinfo(`${playerOneName} Turn`);
+        }
+    }))
+
+    function startGame(){
+        gameInfo.textContent = `${playerOneName} Turn`;
+    }
+
+    const setPlayerinfo = (info) =>{
+        gameInfo.textContent = info;
+    }
+
+    const setPlayerName = (player1, player2) =>{
+        playerOneName = player1;
+        playerTwoName = player2;
+    }
+
+    const disableButton = () =>{
+        clickBtn.forEach(button => button.disabled = true);
+    }
+
+    const enableButtons = () =>{
+        clickBtn.forEach(button => button.disabled = false);
+    }
+
+    return {startGame, disableButton, enableButtons, setPlayerName, setPlayerinfo, clickBtn}
+})();
+
+
+const uiController = (function(){
+    const startBtn = document.querySelector(".Start");
+    const resetBtn = document.querySelector(".Reset");
+    userUI.disableButton();
+    const submitBtn = document.querySelector(".submit-form-btn");
+    const formDialog = document.getElementById("userNameDialog");
+
+    startBtn.addEventListener('click', () =>{
+        clearHitBoxes();
         formDialog.showModal();
         userUI.enableButtons();
     })
 
     submitBtn.addEventListener('click', function(e){
         e.preventDefault();
-        sendUserInfo(document.querySelector("#player1Name").value, document.querySelector("#player2Name").value);
+        sendUserInfo(
+            document.querySelector("#player1Name").value,
+            document.querySelector("#player2Name").value
+        );
         formDialog.close();
         userUI.startGame();
-        
     })
 
     const sendUserInfo = (player1, player2) =>{
-        userUI.setPlayerName(player1, player2)
+        userUI.setPlayerName(player1, player2);
     }
 
-    resetBtn.addEventListener("click", ()=>{
-        clearHitBoxes()
-    } )
+    resetBtn.addEventListener("click", () => clearHitBoxes())
 
     const clearHitBoxes = () =>{
-        userUI.clickBtn.forEach((button) =>{
-            button.textContent=""
-        })
+        userUI.clickBtn.forEach(button => {
+            button.textContent = "";
+            button.disabled = false;
+        });
+        gameBoard.resetBoardArray();
     }
 
-
+    return { clearHitBoxes }
 })();
-
-
